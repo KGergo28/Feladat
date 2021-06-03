a3a22846c4aeb78cb7f828b0ad31a4a9766bd8f8
szt218 <kiss.gergo.18f@szlgbp.hu>
2021-06-03 11:25:10 +0200

    mongo

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.json');
+
+mongoose.connect(config.dbURL);
 
 const app = express();
 
 app.use(express.static('public'));
 
+const schema = new mongoose.Schema({
+	nev: String,
+	szavazatok: Number
+});
+
+const model = mongoose.model('Opciok', schema, 'Opciok');
+
 app.use(express.urlencoded());
 app.post('/add_szav', function(request, response) {
 	console.log(request.body);
 	
+	new model ({
+		nev: request.body.hawaii,
+		szavazatok: 1
+	}).save();
+
 	response.redirect('/');
 });
 
