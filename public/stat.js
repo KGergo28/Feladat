c97fecdeb73d79831b7cf0828a33932c173e7e83
szt218 <kiss.gergo.18f@szlgbp.hu>
2021-06-04 08:15:53 +0200

    stat.html works. Kinda 2.0

--- public/stat.js
+++ public/stat.js
@@ -1,5 +1,14 @@
+const eredmeny = document.getElementById('eredmeny');
+function berak(elem){
+    const p = document.createElement('p');
+    p.innerText = elem.nev + ": " + elem.szavazatok;
+    eredmeny.appendChild(p);
+}
+
 function oldalgener(stat){
     console.log(stat);
+
+    stat.forEach(berak);
 }
 
 fetch('/stat')
